e83331c9941ec26eec10f788066a4b0eea5b6f29
thth3194 <thth3194@trustnhope.com>
2025-10-03 16:58:32 +0900

    fix: 라이브 경기 페이지 레이어 구성 변경

--- src/tennisFolio/src/components/main/LiveEvents.jsx
+++ src/tennisFolio/src/components/main/LiveEvents.jsx
@@ -3,13 +3,10 @@ import './LiveEvents.css';
 import { useNavigate } from 'react-router-dom';
 import Flag from 'react-world-flags';
 import { base_image_url } from '@/constants';
-import { useParams } from 'react-router-dom';
 import SmartImage from './SmartImage';
 
-function LiveEvents({ liveEvents, category: propCategory }) {
+function LiveEvents({ liveEvents }) {
   const navigate = useNavigate();
-  const param = useParams();
-  const category = propCategory || param.category;
 
   const OnClickDetailButton = (event) => {
     navigate(`/liveEvents/${event.rapidId}`);
@@ -17,116 +14,119 @@ function LiveEvents({ liveEvents, category: propCategory }) {
 
   return (
     <div className="live-events">
-      <h1 className="live-title">{category.toUpperCase()} 라이브</h1>
+      <div className="live-title">
+        <div>지금 펼쳐지는 경기, 실시간으로 함께하세요!</div>
+      </div>
       {liveEvents && liveEvents.length === 0 ? (
         <div className="no-events-message">현재 진행 중인 경기가 없습니다.</div>
       ) : (
-        liveEvents &&
-        liveEvents.map((event) => (
-          <div key={event.rapidId} className="eventCard">
-            <div className="tournamentHeader">
-              <div className="tournamentName">{event.tournamentName}</div>
-              <div className="roundName">{event.roundName}</div>
-              <div className="status">{event.status}</div>
-            </div>
-
-            <div className="eventHeader">
-              <div className="teamBlock">
-                <SmartImage
-                  base_url={base_image_url}
-                  imageName={event.homePlayer?.playerImage}
-                  fallbackText={event.homePlayer?.playerName}
-                />
-
-                <div className="teamName">
-                  {event.homePlayer?.playerName}(
-                  {event.homePlayer?.playerRanking})
+        <div className="events-grid">
+          {liveEvents &&
+            liveEvents.map((event) => (
+              <div key={event.rapidId} className="eventCard">
+                <div className="tournamentHeader">
+                  <div className="tournamentName">{event.tournamentName}</div>
+                  <div className="roundName">{event.roundName}</div>
+                  {/* <div className="status">{event.status}</div> */}
                 </div>
-              </div>
 
-              <div className="setScore">
-                {event.homeScore?.current} : {event.awayScore?.current}
-              </div>
+                <div className="eventHeader">
+                  <div className="teamBlock">
+                    <SmartImage
+                      base_url={base_image_url}
+                      imageName={event.homePlayer?.playerImage}
+                      fallbackText={event.homePlayer?.playerName}
+                    />
 
-              <div className="teamBlock">
-                <SmartImage
-                  base_url={base_image_url}
-                  imageName={event.awayPlayer?.playerImage}
-                  fallbackText={event.awayPlayer?.playerName}
-                />
-                <div className="teamName">
-                  {event.awayPlayer?.playerName}(
-                  {event.awayPlayer?.playerRanking})
+                    <div className="teamName">
+                      {event.homePlayer?.playerName}(
+                      {event.homePlayer?.playerRanking})
+                    </div>
+                  </div>
+                  {/* 
+                  <div className="setScore">
+                    {event.homeScore?.current} : {event.awayScore?.current}
+                  </div> */}
+                  <div className="setScore">
+                    {event.homeScore?.point} : {event.awayScore?.point}
+                  </div>
+
+                  <div className="teamBlock">
+                    <SmartImage
+                      base_url={base_image_url}
+                      imageName={event.awayPlayer?.playerImage}
+                      fallbackText={event.awayPlayer?.playerName}
+                    />
+                    <div className="teamName">
+                      {event.awayPlayer?.playerName}(
+                      {event.awayPlayer?.playerRanking})
+                    </div>
+                  </div>
                 </div>
+
+                <div className="eventTable">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>SET1</th>
+                        <th>SET2</th>
+                        <th>SET3</th>
+                        <th>SET4</th>
+                        <th>SET5</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <span style={{ verticalAlign: 'middle' }}>
+                            {event.homePlayer?.playerName}
+                          </span>
+                          <Flag
+                            code={event.homePlayer?.playerCountryAlpha}
+                            style={{
+                              width: '24px',
+                              height: '16px',
+                              verticalAlign: 'middle',
+                              marginLeft: '4px',
+                            }}
+                          />
+                        </td>
+                        {event.homeScore?.periodScore.map((score, index) => (
+                          <td key={index}>{score}</td>
+                        ))}
+                      </tr>
+                      <tr>
+                        <td>
+                          <span style={{ verticalAlign: 'middle' }}>
+                            {event.awayPlayer?.playerName}
+                          </span>
+                          <Flag
+                            code={event.awayPlayer?.playerCountryAlpha}
+                            style={{
+                              width: '24px',
+                              height: '16px',
+                              verticalAlign: 'middle',
+                              marginLeft: '4px',
+                            }}
+                          />
+                        </td>
+                        {event.awayScore?.periodScore.map((score, index) => (
+                          <td key={index}>{score}</td>
+                        ))}
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <button
+                  className="eventButton"
+                  onClick={() => OnClickDetailButton(event)}
+                >
+                  채팅방 입장
+                </button>
               </div>
-            </div>
-
-            <div className="pointScore">
-              {event.homeScore?.point} : {event.awayScore?.point}
-            </div>
-
-            <div className="eventTable">
-              <table>
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>SET1</th>
-                    <th>SET2</th>
-                    <th>SET3</th>
-                    <th>SET4</th>
-                    <th>SET5</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <span style={{ verticalAlign: 'middle' }}>
-                        {event.homePlayer?.playerName}
-                      </span>
-                      <Flag
-                        code={event.homePlayer?.playerCountryAlpha}
-                        style={{
-                          width: '24px',
-                          height: '16px',
-                          verticalAlign: 'middle',
-                          marginLeft: '4px',
-                        }}
-                      />
-                    </td>
-                    {event.homeScore?.periodScore.map((score, index) => (
-                      <td key={index}>{score}</td>
-                    ))}
-                  </tr>
-                  <tr>
-                    <td>
-                      <span style={{ verticalAlign: 'middle' }}>
-                        {event.awayPlayer?.playerName}
-                      </span>
-                      <Flag
-                        code={event.awayPlayer?.playerCountryAlpha}
-                        style={{
-                          width: '24px',
-                          height: '16px',
-                          verticalAlign: 'middle',
-                          marginLeft: '4px',
-                        }}
-                      />
-                    </td>
-                    {event.awayScore?.periodScore.map((score, index) => (
-                      <td key={index}>{score}</td>
-                    ))}
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <button
-              className="eventButton"
-              onClick={() => OnClickDetailButton(event)}
-            >
-              채팅방 입장
-            </button>
-          </div>
-        ))
+            ))}
+        </div>
       )}
     </div>
   );
