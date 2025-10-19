ac1866365fb216e2d338c46daf0433b3b4c9cfc7
AC933040 <AC933040@AC933040>
2025-10-19 15:06:08 +0900

    AP별 Query 개수 측정

---
+++ src/main/java/com/tennisfolio/Tennisfolio/common/monitoring/query/config/QueryCountInterceptor.java
@@ -0,0 +1,81 @@
+package com.tennisfolio.Tennisfolio.common.monitoring.query.config;
+
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.handler.HandlerMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+@AllArgsConstructor
+public class QueryCountInterceptor implements HandlerInterceptor {
+    public static final String UNKNOWN_PATH = "UNKNOWN_PATH";
+
+    private final MeterRegistry meterRegistry;
+
+    /**
+     * 컨트롤러 실행 전: RequestContext 생성 후 ThreadLocal 에 등록
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 1. HTTP 메서드 추출
+        String httpMethod = request.getMethod();
+
+
+        if (!(handler instanceof HandlerMethod)) {
+            System.out.println("Handler is not a HandlerMethod. URI: {}, handler: {}" +  request.getRequestURI() +  handler.getClass().getName());
+        }
+
+
+        // 2. BEST_MATCHING_PATTERN_ATTRIBUTE로부터 path 추출
+        String bestMatchPath =  request.getRequestURI();
+        if (bestMatchPath == null) {
+            bestMatchPath = UNKNOWN_PATH;
+        }
+
+        // 3. ThreadLocal 에 저장
+        RequestContext ctx = RequestContext.builder()
+                .httpMethod(httpMethod)
+                .bestMatchPath(bestMatchPath)
+                .build();
+
+        RequestContextHolder.initContext(ctx);
+
+        return true;
+    }
+
+    /**
+     * 요청 처리 완료 시점: 누적된 쿼리 횟수를 꺼내어 MeterRegistry 에 기록하고 ThreadLocal 정리
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        RequestContext ctx = RequestContextHolder.getContext();
+
+        // 1. 쿼리 횟수를 MeterRegistry 에 기록
+        if (ctx != null) {
+            Map<QueryType, Integer> queryCountByType = ctx.getQueryCountByType();
+            queryCountByType.forEach((queryType, count) -> increment(ctx, queryType, count));
+        }
+
+        // 2. ThreadLocal 해제
+        RequestContextHolder.clear();
+    }
+
+    private void increment(RequestContext ctx, QueryType queryType, Integer count) {
+        DistributionSummary summary = DistributionSummary.builder("app.query.per_request")
+                .description("Number of SQL queries per request")
+                .tag("path", ctx.getBestMatchPath())
+                .tag("http_method", ctx.getHttpMethod())
+                .tag("query_type", queryType.name())
+                .publishPercentiles(0.5, 0.95)
+                .register(meterRegistry);
+
+        summary.record(count);
+    }
+}
--- src/main/java/com/tennisfolio/Tennisfolio/config/WebConfig.java
+++ src/main/java/com/tennisfolio/Tennisfolio/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.tennisfolio.Tennisfolio.config;
 
+import com.tennisfolio.Tennisfolio.common.monitoring.query.config.QueryCountInterceptor;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@AllArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final QueryCountInterceptor queryCountInterceptor;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -21,5 +28,10 @@ public class WebConfig {
                         .maxAge(3600);
             }
         };
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(queryCountInterceptor).addPathPatterns("/**");
     }
 }
