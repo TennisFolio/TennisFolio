475210ac230e0d3b573a95f3abfe586c84ab7b2d
thth3194 <thth3194@trustnhope.com>
2025-10-26 23:20:37 +0900

    fix: 테니스 테스트 타이틀 배너 형식으로 변경

--- src/tennisFolio/src/components/testList/TestHeader.jsx
+++ src/tennisFolio/src/components/testList/TestHeader.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './testHeader.css';
 
 function TestHeader() {
   return (
     <div className="test-header">
-      <h1 className="test-header-title">테니스 테스트</h1>
-      
+      <div>
+        테스트를 통해 나만의 <br></br>테니스 프로필을 만들어보세요!
+      </div>
     </div>
   );
 }
 
-export default TestHeader
+export default TestHeader;
